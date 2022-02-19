ef8c2a00d04a3deb15a08b8cf9cc47b4d861b116
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-19 12:05:43 +0600

    template string

---
+++ es6-intro/template-string.js
@@ -0,0 +1,16 @@
+const mySelf = `My name is Emtiaz Hossain .
+I live in Rangpur city . 
+Now I am learning ES6 .`
+console.log(mySelf);
+
+let count = 22;
+// const myAge = `My age is ` + count
+const myAge = `My age is ${count}`
+console.log(myAge)
+
+const first = 'Rahim'
+const last = 'Choudhury'
+const fullOld = first + ' ' + last
+const fullNew = `${first} ${last}`
+console.log(fullOld)
+console.log(fullNew)
