404abf4d91f4ff07a07439ecc5d18b260cc53bcb
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-19 15:25:45 +0600

    quiz test

---
+++ es6-intro/mcq.js
@@ -0,0 +1,15 @@
+/* 1
+const numbers = [45, 12, 36, 84]
+numbers.unshift(11)
+console.log(numbers) */
+
+/* 2 
+function getSum(a, b = 9) {
+    return a + b;
+}
+console.log(getSum(2, 7)); */
+
+/* 3
+const y = x => x * x; 
+const z = y(5);
+console.log(z) */
