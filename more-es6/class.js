e46c9dd2e4afd9f117f0b2f18298668260d473ed
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-20 22:06:41 +0600

    class

---
+++ more-es6/class.js
@@ -0,0 +1,20 @@
+class Support {
+    name;
+    designation = 'Support Web Dev';
+    address = 'BD';
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+    startSession() {
+        console.log(this.name, 'start a support session');
+    }
+}
+
+const amir = new Support('Aamir Khan', 'BD');
+const salman = new Support('Solaiman Khan', 'Dubai');
+const sharuk = new Support('SRK Khan', 'Dubai');
+const akshay = new Support('Akshay Kumar', 'Dubai');
+amir.startSession();
+salman.startSession();
+console.log(amir, salman, sharuk, akshay);
