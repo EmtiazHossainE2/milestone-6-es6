d4dbc0e497bc71dc7772e22639f193b39c4528b6
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-21 21:59:05 +0600

    recap es6

--- recap-es6/problem-list.js
+++ recap-es6/problem-list.js
@@ -1,1 +1,6 @@
 //১. কখন const আর কখন let দিয়ে ভেরিয়েবল ডিক্লেয়ার করতে হয় সেটা তোমাকে জানতেই হবে। তুমি নিজে নিজে একটা const দিয়ে আরেকটা let দিয়ে ভেরিয়েবল ডিক্লেয়ার করে ফেলো।
+const myName = 'Emtiaz Hossain Emon ' //const variable not change 
+console.log(myName)
+let myAge = 22  // let er value change hote pare kintu const er hobe nah 
+myAge = 23
+console.log(myAge);
