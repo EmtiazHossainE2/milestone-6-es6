e0333d1eb6659bb1847601927e90accbf2ed50a6
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-21 22:27:52 +0600

    solve 2nd problem

--- recap-es6/problem-list.js
+++ recap-es6/problem-list.js
@@ -4,3 +4,14 @@ console.log(myName)
 let myAge = 22  // let er value change hote pare kintu const er hobe nah 
 myAge = 23
 console.log(myAge);
+
+//২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। 
+const mySchool = 'Rangpur Zilla School '
+const myFriend = {
+    name: 'Hajim',
+    home: 'Borobari',
+    phoneNumber: 0177777777
+}
+const { name, home } = myFriend
+const aboutMySchool = `My school name is ${mySchool} . My friend name is ${name} . he lives in ${home}`
+console.log(aboutMySchool)
