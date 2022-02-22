ed983c0551a25162c4b96d4e1658614c7793e9e0
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-22 13:01:53 +0600

    api post

--- api-intro/js/api.js
+++ api-intro/js/api.js
@@ -3,3 +3,14 @@ function jsonApiTest() {
         .then(response => response.json())
         .then(json => console.log(json))
 }
+
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => displayUser(data))
+}
+function displayUser(data) {
+    for (const user of data) {
+        console.log(user)
+    }
+}
