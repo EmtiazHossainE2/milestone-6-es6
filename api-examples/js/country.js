549c76c021608db978dff9d5b173db7dd841a4a7
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-23 19:13:35 +0600

    country details add using  popup

--- api-examples/js/country.js
+++ api-examples/js/country.js
@@ -1,8 +1,9 @@
-function countriesBtn() {
+const countriesBtn = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(response => response.json())
         .then(data => displayCountry(data))
 }
+countriesBtn()
 const displayCountry = data => {
     // for (const country of data) {
     //     console.log(country.name.common)
@@ -10,15 +11,53 @@ const displayCountry = data => {
     const countryContainer = document.getElementById('countries')
     data.forEach(country => {
         // console.log(country)
-        const p = document.createElement('p')
-        p.classList.add('country')
-        p.innerHTML = `
-        Country Name : ${country.name.common} <br>
-        Capital Name : ${country.capital} <br>
-        Borders : ${country.borders} <br>
-        Time Zone : ${country.timezones} <br>
+        const div = document.createElement('div')
+        div.classList.add('col')
+        // div.innerHTML = `
+        // <h4>Country Name : ${country.name.common} </h4>
+        // <p>Capital Name : ${country.capital} </p>
+        // <p>Borders : ${country.borders}</p> 
+        // <p>Time Zone : ${country.timezones} </p>
+        // <button style="
+        //     margin: 10px 5px ;
+        //     padding: 5px 10px;
+        //     border-radius: 10px;
+        //     border: 1px solid green; 
+        //     " onclick="loadCountryDetails('${country.name.common}')">Details </button>
+        // `
+        div.innerHTML = `
+        <div class="card h-100">
+            <div class="card-body">
+                <h4>Country Name : ${country.name.common} </h4>
+                <p>Capital Name : ${country.capital} </p>
+                <p>Borders : ${country.borders}</p> 
+                <p>Time Zone : ${country.timezones} </p>
+                
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                    onclick="loadCountryDetails('${country.name.common}')">
+                    Details
+                </button>
+            </div>
+        </div>
         `
-        countryContainer.appendChild(p)
+        countryContainer.appendChild(div)
     })
 }
+const loadCountryDetails = details => {
+    // const url = `https://restcountries.com/v3.1/name/${details}`
+    fetch(`https://restcountries.com/v3.1/name/${details}`)
+        .then(response => response.json())
+        .then(data => displayCountryDetails(data[0]))
+}
+
+const displayCountryDetails = country => {
+    const countryDetails = document.getElementById('country-details')
+    countryDetails.innerHTML = `
+    <h4>Country Name : ${country.name.common} </h4>
+        <p>Capital Name : ${country.capital} </p> 
+        <p>Time Zone : ${country.timezones} </p>
+        <img src="${country.flags.png}">
+    `
+}
+
 
