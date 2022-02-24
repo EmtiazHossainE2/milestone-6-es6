d95876a951019b1be9d85bd138f1b924c37c6c61
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-24 23:25:31 +0600

    random users

--- recap-api/js/api-checklist.js
+++ recap-api/js/api-checklist.js
@@ -53,7 +53,7 @@ const displayComment = comments => {
 
 //৪. তুমি যে ডাটা লোড করেছো। বা ডাটা ওয়েবসাইট এ দেখাচ্ছ। সেই কোড এর মধ্যে arrow ফাংশন ইউজ করতে পারতেছো কিনা। যখন লুপ চালাচ্ছ সেখানে forEach ইউজ করতে পারতেছো কিনা। 
 
-const loadComment = () => {
+/* const loadComment = () => {
     fetch('https://jsonplaceholder.typicode.com/comments')
         .then(res => res.json())
         .then(data => displayComment(data.slice(0, 20)))
@@ -74,4 +74,66 @@ const displayComment = comments => {
     `
         comment.appendChild(div)
     })
+} */
+
+//৫. ডাইনামিক ডাটা লোড। কোন একটা কমেন্ট এ ক্লিক করলে (কমেন্ট এর div এ বা কোন একটা বাটন এ )সেই কমেন্ট এর আইডি নিয়ে সেটা api এর url এ বসিয়ে (ডাইনামিকভাবে টেমপ্লেট স্ট্রিং দিয়ে) সেই ডাটা লোড করে। সেই ডাটা ওয়েবসাইট এ দেখাতে পারতেছো কিনা।
+
+/* const loadComment = () => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(res => res.json())
+        .then(data => displayComment(data.slice(0, 20)))
+}
+loadComment()
+
+const displayComment = comments => {
+    comments.forEach(showComment => {
+        // console.log(showComment)
+        const comment = document.getElementById('comment')
+        const div = document.createElement('div')
+        div.classList.add('comment')
+        div.innerHTML = `
+        <h3>Comment : ${showComment.id}</h3>
+        <p>Title : ${showComment.name}</p>
+        <p>User Email : ${showComment.email}</p>
+        <p>User Comment : ${showComment.body}</p>
+    `
+        comment.appendChild(div)
+    })
+} */
+
+
+//৬. randomuser এর ওয়েবসাইট এ গিয়ে (randomuser.me) এ গিয়ে সেখান থেকে ডাটা লোড করবে। তারপর ইউজারের ছবি দেখাবে। শুধু সেটাও না। ইউজারের location এর মধ্যে যত কিছু আছে। সব দেখাবে ওয়েবসাইট এ। অর্থাৎ street, city, coordinates, timezone যেকোন একভাবে দেখলেই হবে। তবে দেখাতে হবে। 
+
+fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => displayUser(data))
+
+const displayUser = users => {
+    const results = users.results
+    userDisplay(results)
+
+}
+function userDisplay(results) {
+    results.forEach(user => {
+        console.log(user.location)
+        const randomUser = document.getElementById('random-user')
+        randomUser.innerHTML = `
+            <img src="${user.picture.large}">
+            <h4>City : ${user.location.city}</h4>
+            <p>Coordinates : ${user.location.coordinates.latitude} , ${user.location.coordinates.longitude}</p>
+            <p>Timezone : ${user.location.timezone.offset} , ${user.location.timezone.description}</p>
+            <button onclick="randomBtn()" class="btn btn-success">Random User</button>
+        `
+    })
+}
+const randomBtn = () => {
+    fetch('https://randomuser.me/api/')
+        .then(res => res.json())
+        .then(data => displayBtn(data))
+}
+const displayBtn = something => {
+    // console.log(something.results)
+    const results = something.results
+    userDisplay(results)
+
 }
