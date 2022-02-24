36b176c2961dec72e6df2ab7041e52b1ab31b044
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-24 17:40:24 +0600

    mealdb complete with modal box

--- api-examples/js/mealdb.js
+++ api-examples/js/mealdb.js
@@ -1,30 +1,53 @@
-const searchFood = () => {
+const searchFood = async () => {
     const searchField = document.getElementById('search-field')
     const searchValue = searchField.value;
     // console.log(searchValue)
     searchField.value = ''
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`
-    fetch(url)
-        .then(response => response.json())
-        .then(data => displaySearchResult(data.meals))
+    if (searchValue == '') {
+        alert('Write your favourite food ')
+    }
+    else {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`;
+        const response = await fetch(url)
+        const data = await response.json()
+        displaySearchResult(data.meals)
+        // fetch(url)
+        //     .then(response => response.json())
+        //     .then(data => displaySearchResult(data.meals))
+    }
+
 }
 const displaySearchResult = meals => {
+    console.log(meals)
     const searchResult = document.getElementById('search-result')
-    meals.forEach(meal => {
-        console.log(meal)
+    // searchResult.innerText = '' ;
+    searchResult.textContent = '';
+    if (meals == null) {
+        // alert('No Result Found')
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
-            <div onclick="loadMeadId(${meal.idMeal})" class="card h-100">
-                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${meal.strMeal.slice(0, 25)}</h5>
-                    <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
-                </div>
-            </div>
-        ` ;
+                <h4 class="text-light mt-5 text-center">No Result Found</h4>
+            ` ;
         searchResult.appendChild(div)
-    })
+    }
+    else {
+        meals.forEach(meal => {
+            console.log(meal)
+            const div = document.createElement('div')
+            div.classList.add('col')
+            div.innerHTML = `
+                <div onclick="loadMeadId(${meal.idMeal})" data-bs-toggle="modal" data-bs-target="#exampleModal" class="card h-100">
+                    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${meal.strMeal.slice(0, 25)}</h5>
+                        <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+                    </div>
+                </div>
+            ` ;
+            searchResult.appendChild(div)
+        })
+    }
 }
 const loadMeadId = mealId => {
     // const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
@@ -33,8 +56,9 @@ const loadMeadId = mealId => {
         .then(data => displayDetails(data.meals[0]))
 }
 const displayDetails = meal => {
-    console.log(meal)
+    // console.log(meal)
     const mealDetails = document.getElementById('meal-details')
+    mealDetails.textContent = ''
     const div = document.createElement('div')
     div.classList.add('card')
     div.innerHTML = `
