748cb5681a9c2d7a47907959dcf070f5a197bdf4
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-27 02:31:43 +0600

    destructuring

--- recap-milestone-6/checklist.js
+++ recap-milestone-6/checklist.js
@@ -86,3 +86,15 @@ this ==> class er vitore obj er property gulo access korar jonne this use kora h
 
 */
 
+//৬. সিম্পল একটা জাভাস্ক্রিপ্ট অবজেক্ট এর কোন একটা প্রোপার্টিকে ভেরিয়েবল হিসেবে ডিক্লেয়ার করার জন্য destructuring ইউজ করো। array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর সেকেন্ড পজিশন এর উপাদান কে destructuring করে 'balance' নামক একটা ভেরিয়েবল এ রাখবে। 
+
+const myObj = {
+    name: " Emtiaz Emon ",
+    money: 3000,
+    home: 'rangpur ',
+    village: 'pirjabad'
+}
+const { money } = myObj
+console.log(money)
+
+//
