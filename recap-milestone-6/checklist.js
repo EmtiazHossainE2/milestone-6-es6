240af9b137f739ecf35c6856f2a7ae93e446cae5
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-27 02:03:59 +0600

    multy perameter use arrow function

--- recap-milestone-6/checklist.js
+++ recap-milestone-6/checklist.js
@@ -18,3 +18,50 @@ const mySelf = {
 console.log(mySelf.bestFriend.name)
 console.log(mySelf.phoneNumbers[1])
 console.log(mySelf.food(500))
+
+//২. একটা টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার করো। সেটার মধ্যে উপরের অবজেক্ট এর একটা তিনটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। এর মধ্যে একটা প্রপার্টি সেট করবে--যেটা নেস্টেড অবজেক্ট আছে সেটার প্রপার্টি থেকে। আরেকটা প্রপার্টি হবে যে প্রপার্টি এর মান একটা array সেটার সেকেন্ড পজিশন এর উপাদান দিয়ে। আর বাকি একটা তোমার ইচ্ছা মতো বসাতে পারো। 
+
+const friends = `
+    money: 5000,
+    bestFriend: {
+        name: 'Rion ',
+        home: 'Pirgonj',
+        study: 'RMC',
+        age: 23
+    },
+    food: function (exp) {
+        this.money = this.money - exp
+        return this.money
+    }
+`
+
+//৩.১. শূন্য প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে যেটা ৮৯ রিটার্ন করবে। 
+const number89 = () => 89
+console.log(number89())
+
+//৩.২. এক প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন ডিক্লেয়ার করবে। এই ফাংশনের কাজ হবে যে প্যারামিটার নিবে সেটাকে ৭ দিয়ে ভাগ করে ভাগফল রিটার্ন করবে। 
+const singlePera = x => x / 7
+console.log(singlePera(49))
+
+// const fiveTimes = num => num * 5;
+// const value = fiveTimes(15);
+// console.log(value)
+
+//৩.৩ দুই প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে। এই ফাংশনের কাজ হবে যে দুইটা প্যারামিটার ইনপুট নিবে। সেই দুইটা প্যারামিটারকে যোগ করে যোগফলকে দুই দিয়ে ভাগ করে ভাগফল রিটার্ণ করে দাও। 
+
+const doublePera = (x, y) => {
+    const sum = x + y;  // 10+20 = 30 
+    const divi = sum / 2; // 30/ 2 = 15 
+    return divi;
+}
+console.log(doublePera(10, 20))
+
+//৩.৪ একাধিক লাইন ওয়ালা অ্যারো ফাংশন লিখো। সেটাতে দুইটা প্যারামিটার নিবে। ওই arrow ফাংশনটা হবে অনেকগুলা লাইনের। সেখানে প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ৭ যোগ করবে তারপর যোগফল দুইটাকে আবার যোগ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো। 
+
+const multyPera = (x, y) => {
+    const sumX = x + 7;  // 10+7 = 17
+    const sumY = y + 7; // 20+ 7 = 27 
+    const totalSum = sumX + sumY; //17+27 = 44
+    return totalSum
+}
+console.log(multyPera(10, 20))
