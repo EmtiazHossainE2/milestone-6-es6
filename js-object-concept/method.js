388246ba50db6f707d24ac6961f05c43d5699059
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-26 15:17:17 +0600

    method js

--- js-object-concept/method.js
+++ js-object-concept/method.js
@@ -1,11 +1,17 @@
 const mySelf = {
     name: 'Emtiaz Hossain',
     lastName: 'Emon',
+    money: 5000,
     age: 22,
     bestFriends: ['Rion', 'Dipta'],
     dream: function () {
         console.log(this.name, 'dream is software engineer');
         console.log(this.bestFriends[0], ' is a Doctor');
+    },
+    food: function (exp, gift) {
+        this.money = this.money - exp - gift;
+        return this.money
     }
 }
 mySelf.dream()
+console.log(mySelf.food(2000, 500))
